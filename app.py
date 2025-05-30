50d037e5bf7c1f17f33ed52c91d0df33142c728b
loplopel <rodrigo.franco@nacar.com.br>
2025-05-30 09:49:30 -0300

    feat: marcar exercício como concluído

--- app.py
+++ app.py
@@ -3,41 +3,36 @@ import json
 import os
 
 app = Flask(__name__, static_url_path='/static', static_folder='static')
-
-
 DATA_PATH = 'data/treinos.json'
-
 
 def carregar_treinos():
     with open(DATA_PATH, 'r', encoding='utf-8') as f:
         return json.load(f)
 
-
 def salvar_treinos(data):
     with open(DATA_PATH, 'w', encoding='utf-8') as f:
         json.dump(data, f, indent=2, ensure_ascii=False)
-
 
 @app.route('/')
 def index():
     treinos = carregar_treinos()
     return render_template('index.html', treinos=treinos)
 
-
 @app.route('/treino/<dia>', methods=['GET', 'POST'])
 def treino(dia):
     treinos = carregar_treinos()
 
     if request.method == 'POST':
-        # Editar carga
         index = int(request.form['index'])
-        nova_carga = request.form['nova_carga']
-        treinos[dia][index]['carga'] = nova_carga
+        if 'nova_carga' in request.form:
+            nova_carga = request.form['nova_carga']
+            treinos[dia][index]['carga'] = nova_carga
+        elif 'concluido' in request.form:
+            treinos[dia][index]['concluido'] = request.form.get('concluido') == 'on'
         salvar_treinos(treinos)
         return redirect(url_for('treino', dia=dia))
 
     return render_template('treino.html', dia=dia, exercicios=treinos.get(dia, []))
-
 
 @app.route('/adicionar/<dia>', methods=['POST'])
 def adicionar_exercicio(dia):
@@ -47,14 +42,12 @@ def adicionar_exercicio(dia):
         "imagem": request.form['imagem'],
         "series": request.form['series'],
         "carga": request.form['carga'],
-        "obs": request.form['obs']
+        "obs": request.form['obs'],
+        "concluido": False
     }
     treinos[dia].append(novo)
     salvar_treinos(treinos)
     return redirect(url_for('treino', dia=dia))
-
-
-import os
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
