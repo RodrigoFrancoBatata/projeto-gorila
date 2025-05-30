69486c0e330ba02bb3c9e33ed96224bb44f8204e
loplopel <rodrigo.franco@nacar.com.br>
2025-05-30 09:28:14 -0300

    Melhora suporte offline no PWA

--- static/serviceWorker.js
+++ static/serviceWorker.js
@@ -1,19 +1,50 @@
+const CACHE_NAME = 'gorila-v2';
+const urlsToCache = [
+  '/',
+  '/static/style.css',
+  '/static/manifest.json',
+  '/static/icon-192.png',
+  '/static/icon-512.png'
+];
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open("gorila-cache").then((cache) =>
-      cache.addAll([
-        "/",
-        "/static/style.css",
-        "/static/icon-192.png",
-        "/static/icon-512.png"
-      ])
-    )
+// Instala o service worker e faz cache básico
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
+// Ativa nova versão e limpa cache antigo
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
   );
 });
+
+// Intercepta requisições e tenta cache primeiro
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return (
+        response ||
+        fetch(event.request).catch(() =>
+          new Response("Sem conexão. Acesse com internet ao menos uma vez.", {
+            headers: { "Content-Type": "text/plain" }
+          })
+        )
+      );
+    })
+  );
+});
+
+
